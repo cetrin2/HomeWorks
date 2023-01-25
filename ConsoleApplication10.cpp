c4d0b6a283b9061ef4173cf540a65294a5f1db6d
cetrin2 <38029626+cetrin2@users.noreply.github.com>
2023-01-25 23:48:26 +0300

    Add files via upload

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,58 @@
+
+#include "stdafx.h"
+
+
+int main()
+{
+	//Создаем массив
+	int arr[7];
+
+	printf("Enter the values of array\n");
+
+	//Получаем значения от пользователя
+	for (int i = 0; i < 7; i++)
+	{
+		scanf_s("%i", &arr[i]);
+	}
+
+	printf("you have entered values: ");
+
+	//Выводим результат
+	for (int i = 0; i < 7; i++)
+	{
+		printf("%2i\n", arr[i]);
+	}
+
+
+	int n;				//Переменная для хранения размера массива
+	int arr_b[100];		//Создаем массив большого размера
+
+	//Получаем данные от пользователя
+	printf("Enter the size of array not more than 100:");
+	scanf_s("%i", &n);
+
+	//Проверяем чтобы размер массива был не больше максимального размера
+	if (n <= 100)
+	{
+		printf("Enter the values of array\n");
+
+		//Получаем значения от пользователя
+		for (int i = 0; i < n; i++)
+		{
+			scanf_s("%i", &arr_b[i]);
+		}
+
+		printf("you have entered values: ");
+
+		//Выводим значения
+		for (int i = 0; i < n; i++)
+		{
+			printf("%2i", arr_b[i]);
+		}
+	}
+	else
+	{
+		printf("The size of array is more than 100");
+	}
+}
+
