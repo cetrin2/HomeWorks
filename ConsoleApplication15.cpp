be74311c5638ac024b0e0fbd92b1b2da448e62ea
cetrin2 <38029626+cetrin2@users.noreply.github.com>
2023-02-12 16:32:39 +0300

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+
+
+int main()
+{
+	//Объявляем массив
+	int arr[100][100];
+
+	//Объявляем переменные для размера массива
+	int rows, cols;
+
+	//Запрашиваем количество строк
+	printf("Enter quantity of rows: ");
+	scanf_s("%i", &rows);
+	printf("\n\n");
+
+	//Запрашиваем количество столбцов
+	printf("Enter quantity of cols: ");
+	scanf_s("%i", &cols);
+	printf("\n\n");
+
+	//Наполняем массив диапазон от 0 до 10
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			scanf_s("%i", &arr[i][j]);
+		}
+	}
+
+	//Выводим результат исходного массива
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("%4i", arr[i][j]);
+		}
+
+		printf("\n");
+	}
+
+	printf("\n\n");
+
+	//Далаем инверсию главной диагонали
+	for (int i = 0; i < rows / 2; i++)
+	{
+		int mRow = rows - i - 1;
+		int mColumn = i;
+
+		int tmp = arr[mRow][mColumn];
+
+		arr[mRow][mColumn] = arr[mColumn][mRow];
+
+		arr[mColumn][mRow] = tmp;
+	}
+
+	//Выводим результат исходного массива
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("%4i", arr[i][j]);
+		}
+
+		printf("\n");
+	}
+}
