b06442430fadbb39f21fe91042732489313622c3
cetrin2 <38029626+cetrin2@users.noreply.github.com>
2023-02-15 21:48:32 +0300

    Add files via upload

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,44 @@
+
+#include "stdafx.h"
+#include "windows.h"
+
+int main()
+{
+	//Инициализируем массив и переменные
+	char str[100];
+	int c, i, j;
+
+	//Получаем строку
+	printf("Enter a string\n");
+	gets_s(str);
+
+	//Выводим исходный результат
+	puts(str);
+
+	//Удаляем первый символ
+	for (int n = 0; n < strlen(str); n++)
+	{
+		str[n] = str[n + 1];
+	}
+
+	//Выводим результат без первого символа
+	puts(str);
+
+	//Получаем новую строку
+	printf("Enter a next string\n");
+	gets_s(str);
+
+	int length = strlen(str);
+	
+	//Меняем местами символы
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		c = str[i];
+		str[i] = str[j];
+		str[j] = c;
+	}
+
+	puts(str);
+
+	system("pause");
+}
